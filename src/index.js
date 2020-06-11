0c762c5571974949da80377c5abe155d6787a06d
jhonysmith <evesnin68@gmail.com>
2020-06-11 12:16:54 +0300

    1.3.3 Правит замечания

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from "react-dom";
 
 import App from "./components/app/app.jsx";
 
-const init = () => {
-  const promoFilmData = {
-    promoTitle: `The Grand Budapest Hotel`,
-    promoGenre: `Drama`,
-    promoDateRelease: `2014`
-  };
-
-  ReactDOM.render(
-      <App promoTitle={promoFilmData.promoTitle} promoGenre={promoFilmData.promoGenre} promoReleaseDate={promoFilmData.promoDateRelease}/>,
-      document.querySelector(`#root`)
-  );
+const PromoFilmData = {
+  PROMO_TITLE: `The Grand Budapest Hotel`,
+  PROMO_GENRE: `Drama`,
+  PROMO_DATE_RELEASE: `2014`
 };
 
-init();
+ReactDOM.render(
+    <App promoTitle={PromoFilmData.PROMO_TITLE} promoGenre={PromoFilmData.PROMO_GENRE} promoReleaseDate={PromoFilmData.PROMO_DATE_RELEASE}/>,
+    document.querySelector(`#root`));

c0792c2fa1648564e807f09e8ae402058ecb39ee
JhonySmith <evesnin68@gmail.com>
2020-06-19 13:16:41 +0300

    2.2.1 Добавлены снэпшоты для компонентов

---
+++ src/components/main/main.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const TestData = {
+  PROMO_FILM: {
+    PROMO_TITLE: `The Grand Budapest Hotel`,
+    PROMO_GENRE: `Drama`,
+    PROMO_DATE_RELEASE: 2014
+  },
+  FILMS: [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`],
+};
+
+describe(`Main page unit tests`, () => {
+  it(`Render main page`, () => {
+    const tree = renderer
+      .create(
+          <Main
+            promoTitle={TestData.PROMO_FILM.PROMO_TITLE}
+            promoGenre={TestData.PROMO_FILM.PROMO_GENRE}
+            promoReleaseDate={TestData.PROMO_FILM.PROMO_DATE_RELEASE}
+            films={TestData.FILMS}
+          />
+      ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
